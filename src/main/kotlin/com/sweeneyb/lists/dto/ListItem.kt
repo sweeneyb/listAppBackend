c8e01e13478e9d9235113974ed3a5c448dbb5656
sweeneyb <tab126@gmail.com>
2018-05-04 16:10:45 -0500

    Adds db migration makes IDs UUIDs

--- src/main/kotlin/com/sweeneyb/lists/dto/ListItem.kt
+++ src/main/kotlin/com/sweeneyb/lists/dto/ListItem.kt
@@ -1,16 +1,18 @@
 package com.sweeneyb.lists.dto
 
+import org.hibernate.id.GUIDGenerator
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import java.time.LocalTime
+import java.util.*
 import javax.persistence.Entity
 import javax.persistence.Id
 
 @Entity
 data class ListItem(
-        @Id val id: LocalTime=LocalTime.now(),
+        @Id val id: UUID = UUID.randomUUID(),
         val isDone: Boolean = false,
         val task: String = "")
 
 @RepositoryRestResource
-interface ListItemRepository : PagingAndSortingRepository<ListItem, LocalTime>
+interface ListItemRepository : PagingAndSortingRepository<ListItem, UUID>
